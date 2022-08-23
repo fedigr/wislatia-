cd38ec49df99859aba158a7419454c737d21e624
fedigr <101499063+fedigr@users.noreply.github.com>
2022-08-23 11:59:58 +0100

    Form validation
    Validation form

---
+++ Formvalidatia.js
@@ -0,0 +1,18 @@
+Let m = document.getElementById("name");
+Let d = document.createElement("error");
+function error(){
+}
+function validate(m){
+if(m.value.length!=0){
+}
+else{
+error()
+}
+
+
+// There are a big problem in this code 
+// Tchange after minute 
+/* 
+Form validation 
+*/
+
